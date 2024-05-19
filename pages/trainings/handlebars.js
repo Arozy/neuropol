c7a342615c0dd569288e06e874625355b710ce07
arozy <ale.roz1921@wp.pl>
2024-05-19 12:26:30 +0200

    zmiany 17/05/2024 -> sekcja szkolenia, formularz, mechanizm wyboru (WIP)

--- pages/trainings/handlebars.js
+++ pages/trainings/handlebars.js
@@ -1,3 +1,11 @@
+Handlebars.registerHelper('json', function (context) {
+    return JSON.stringify(context);
+});
+
+onchange = () => {
+    _saveData();
+}
+
 function getShared(selection) {
     const data = {
         tel: '+48 600 02 99 07',
@@ -39,6 +47,8 @@ function getShared(selection) {
 function runFillers() {
     const homepage = new Page();
     homepage.fillHeaderTemplate();
+    homepage.fillTrainingsTemplate();
+    homepage.fillFormTemplate();
     homepage.fillFooterTemplate();
 }
 
@@ -71,6 +81,100 @@ class Page {
             ],
         };
         this.useFiller(data, "header");
+    }
+
+    fillTrainingsTemplate() {
+        const data = {
+            heading: 'Oferujemy wiele szkoleń z zakresu Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            description: 'W Neuropol nie tylko leczymy, ale też edukujemy o zdrowiu. ' +
+                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+                'Distinctiodoloribus enim explicabo fuga ipsa numquam pariatur perspiciatisrepudiandae temporibus veniam.',
+            sections: [
+                {
+                    name: 'pierwsza-pomoc',
+                    heading: 'Szkolenia z pierwszej pomocy',
+                    columns: [
+                        {
+                            type: 'podstawowy',
+                            title: 'Pakiet Podstawowy',
+                            price: 90,
+                            items: [
+                                'RKO u dorosłych i dzieci',
+                                'wezwanie pomocy',
+                                'ułożenie w pozycji bocznej bezpiecznej'
+                            ],
+                            out: [
+                                'pierwsza pomoc w przypadku omdleń',
+                                'pierwsza pomoc w przypadku udarów',
+                                'pierwsza pomoc w przypadku poparzeń',
+                                'pierwsza pomoc w zadławieniach',
+                                'opatrywanie po złamaniu',
+                                'opatrywanie po pogryzieniu',
+                                'opatrywanie po postrzale',
+                                'tamowanie krwotoków',
+                                'urazy kostno-stawowe',
+                                'możliwość rozbudowania pakietu'
+                            ]
+                        },
+                        {
+                            type: 'standard',
+                            title: 'Pakiet Standard',
+                            price: 100,
+                            items: [
+                                'RKO u dorosłych i dzieci',
+                                'wezwanie pomocy',
+                                'ułożenie w pozycji bocznej bezpiecznej',
+                                'pierwsza pomoc w przypadku omdleń',
+                                'pierwsza pomoc w przypadku udarów',
+                                'pierwsza pomoc w przypadku poparzeń',
+                            ],
+                            out: [
+                                'pierwsza pomoc w zadławieniach',
+                                'opatrywanie po złamaniu',
+                                'opatrywanie po pogryzieniu',
+                                'opatrywanie po postrzale',
+                                'tamowanie krwotoków',
+                                'urazy kostno-stawowe',
+                                'możliwość rozbudowania pakietu'
+                            ]
+                        },
+                        {
+                            type: 'premium',
+                            title: 'Pakiet Premium',
+                            price: 120,
+                            items: [
+                                'RKO u dorosłych i dzieci',
+                                'wezwanie pomocy',
+                                'ułożenie w pozycji bocznej bezpiecznej',
+                                'pierwsza pomoc w przypadku omdleń',
+                                'pierwsza pomoc w przypadku udarów',
+                                'pierwsza pomoc w przypadku poparzeń',
+                                'pierwsza pomoc w zadławieniach',
+                                'opatrywanie po złamaniu',
+                                'opatrywanie po pogryzieniu',
+                                'opatrywanie po postrzale',
+                                'tamowanie krwotoków',
+                                'urazy kostno-stawowe',
+                                'możliwość rozbudowania pakietu'
+                            ]
+                        }
+                    ],
+                    additionalInformation: [
+                        'Cena może się różnić w zależności od ilości uczestników. Powyżej 50 osób doliczamy rabat 5%, powyżej 100, 10%',
+                        'Kurs kończy się wydaniem certyfikatów uczestnikom'
+                    ]
+                }
+            ]
+
+        }
+        this.useFiller(data, 'training');
+    }
+
+    fillFormTemplate() {
+        const data = {
+            heading: 'Uzyskaj ofertę dla swojej firmy',
+        }
+        this.useFiller(data, 'purchase');
     }
 
     fillFooterTemplate() {
@@ -117,3 +221,97 @@ class Page {
         this.useFiller(data, 'footer');
     }
 }
+
+async function readData() {
+    await new Promise((resolve) => {
+        if (document.readyState === 'complete') {
+            resolve();
+        } else {
+            document.addEventListener('DOMContentLoaded', resolve);
+        }
+    });
+
+    const formData = JSON.parse(localStorage.getItem('purchase_form_data'));
+    const form = document.getElementById('purchase-form');
+    if (formData) {
+        Object.entries(formData).forEach(([key, value]) => {
+            const element = form.elements.namedItem(key)
+            if (element) {
+                element.value = value;
+            }
+        })
+    }
+}
+
+function handlePacketSelection(packet, training) {
+    const amountInput = document.querySelector(`#amount_${packet}_${training}`);
+    const selectionInput = document.querySelector('[data-selection-input]');
+    const selectionButton = document.querySelector(`[data-selection-button="${packet} | ${training}"]`)
+    _announceButtonClick(selectionButton);
+    let currentlySelectedPackets = JSON.parse(localStorage.getItem('selectedPackets')) || [];
+    if (!currentlySelectedPackets.includes({packet, training, amount: Number(amountInput.value)})) {
+        currentlySelectedPackets.push(
+            {
+                packet: packet,
+                training: training,
+                amount: Number(amountInput.value)
+            }
+        )
+    } else {
+        const toBeUpdatedElement = currentlySelectedPackets.find(element => element.packet === packet && element.training === training);
+        toBeUpdatedElement.amount = amountInput.value;
+    }
+
+    currentlySelectedPackets = Object.values(currentlySelectedPackets.reduce((elem, {packet, training, amount}) => {
+        const key = `${packet} | ${training}`;
+        if (!elem[key]) {
+            elem[key] = {packet, training, amount: 0};
+        }
+        elem[key].amount = Number(amount);
+        return elem;
+    }, {}));
+
+    localStorage.setItem('selectedPackets', JSON.stringify(currentlySelectedPackets));
+    selectionInput.value = '';
+
+    JSON.parse(localStorage.getItem('selectedPackets')).forEach(element => {
+        if (element.amount === 0) return;
+        const elementChips = `${element.packet} | ${element.training} (${element.amount}), `;
+        selectionInput.value += elementChips;
+    })
+
+    _saveData();
+}
+
+function _announceButtonClick(selectionButton) {
+    selectionButton.innerHTML = 'Wybrano!';
+    setTimeout(() => {
+        selectionButton.innerHTML = 'Wybieram'
+    }, 2000)
+}
+
+function _saveData() {
+    const form = document.getElementById('purchase-form');
+    let elements = [...form.elements];
+    let savedData = [];
+
+    elements.forEach(element => {
+        savedData.push({
+            name: element.name,
+            value: element.value
+        })
+    })
+
+    savedData = elements.reduce((elem, {name, value}) => {
+        if (!elem[name]) {
+            elem[name] = {name, value};
+        }
+        elem[name].value = value;
+        return elem;
+    }, {})
+
+    localStorage.removeItem('purchase_form_data');
+    localStorage.setItem('purchase_form_data', JSON.stringify(savedData));
+}
+
+readData();
