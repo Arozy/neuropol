09c680d36d47ccc417762accbd9ff2dd09480762
arozy <ale.roz1921@wp.pl>
2024-05-30 16:53:40 +0200

    30/05/2024 - dodanie cennika

--- pages/services/handlebars.js
+++ pages/services/handlebars.js
@@ -1,8 +1,52 @@
 function runFillers() {
     const services = new Services('services');
     services.fillHeaderTemplate();
+    services.fillPricingTemplate();
     services.fillFooterTemplate();
 }
 
 class Services extends Page {
+    fillPricingTemplate() {
+        const data = {
+            heading: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure provident quae quam quo tempore ullam.',
+            class: 'col-lg-8 col-md-12 col-12 offset-2',
+            columns: [
+                //             {
+                //                 icon: 'icofont icofont-price',
+                //                 title: 'Usługi NFZ',
+                //                 items: [
+                //                     {
+                //                         name: 'Testowe',
+                //                     },
+                //                     {
+                //                         name: 'Testowe',
+                //                     },
+                //                     {
+                //                         name: 'Testowe',
+                //                     }
+                //                 ]
+                //             },
+                {
+                    icon: 'fa fa-tag',
+                    title: 'Usługi Prywatne',
+                    items: [
+                        {
+                            name: 'Testowe',
+                            value: '100zł'
+                        },
+                        {
+                            name: 'Testowe',
+                            value: '200zł'
+                        },
+                        {
+                            name: 'Testowe',
+                            value: '130zł'
+                        }
+                    ]
+                }
+            ]
+        };
+        this.useFiller(data, 'pricing');
+    }
 }
